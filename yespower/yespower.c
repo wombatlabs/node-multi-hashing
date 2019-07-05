a944b99d15f1ed94c5c733208a6a86eebf0df0d3
Min Khang Aung <minkcrypto@gmail.com>
2019-07-06 06:21:11 +0900

    Add CPUpower support

--- yespower/yespower.c
+++ yespower/yespower.c
@@ -107,3 +107,14 @@ void yespower_0_5_R32_hash(const char *input, char *output)
         yespower_tls((const uint8_t *) input, 80, &params, (yespower_binary_t *) output);
 }
 
+void cpupower_hash(const char *input, char *output)
+{
+        yespower_params_t params = {
+                .version = YESPOWER_1_0,
+                .N = 2048,
+                .r = 32,
+                .pers = "CPUpower: The number of CPU working or available for proof-of-work mining",
+                .perslen = 73
+        };
+        yespower_tls((const uint8_t *) input, 80, &params, (yespower_binary_t *) output);
+}
