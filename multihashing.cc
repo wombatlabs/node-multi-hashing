dcc62a66383f446298b7fd66c3526a01ceebe5ef
wahaobi <40647643+wahaobi@users.noreply.github.com>
2019-10-14 11:04:30 +0800

    Update multihashing.cc

--- multihashing.cc
+++ multihashing.cc
@@ -528,6 +528,27 @@ DECLARE_FUNC(cpupower){
     SET_BUFFER_RETURN(output, 32);
 }
 
+DECLARE_FUNC(power2b) {
+	DECLARE_SCOPE;
+
+	if (args.Length() < 1)
+		RETURN_EXCEPT("You must provide one argument.");
+
+	Local<Object> target = args[0]->ToObject();
+
+	if (!Buffer::HasInstance(target))
+		RETURN_EXCEPT("Argument should be a buffer object.");
+
+
+	char * input = Buffer::Data(target);
+	char output[32];
+
+
+	power2b_hash(input, output);
+
+	SET_BUFFER_RETURN(output, 32);
+}
+
 DECLARE_FUNC(yescrypt){
     DECLARE_SCOPE;
 
@@ -606,6 +627,7 @@ DECLARE_INIT(init) {
     NODE_SET_METHOD(exports, "yespower_0_5_R24", yespower_0_5_R24);
     NODE_SET_METHOD(exports, "yespower_0_5_R32", yespower_0_5_R32);
     NODE_SET_METHOD(exports, "cpupower", cpupower);
+	NODE_SET_METHOD(exports, "power2b", power2b);
     NODE_SET_METHOD(exports, "yescrypt", yescrypt);
     NODE_SET_METHOD(exports, "yescrypt_bitzeny", yescrypt_bitzeny);
 }
