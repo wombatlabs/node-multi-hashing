756387c8715dc3871f58dec93d0ae684c4ad0392
ROZ <35634920+ROZ-MOFUMOFU-ME@users.noreply.github.com>
2019-11-10 02:10:30 +0900

    Update yespower-opt-1.0.1.c

--- yespower-1.0.1/yespower-opt-1.0.1.c
+++ yespower-1.0.1/yespower-opt-1.0.1.c
@@ -1015,7 +1015,7 @@ static void smix(uint8_t *B, size_t r, uint32_t N,
 #define smix1 smix1_1_0
 #define smix2 smix2_1_0
 #define smix smix_1_0
-#include "yespower-opt.c"
+#include "yespower-opt-1.0.1.c"
 #undef smix
 
 /**
