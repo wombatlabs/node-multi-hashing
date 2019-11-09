8dbeea6fee31673c35b679be39d807f77e888ace
ROZ <35634920+ROZ-MOFUMOFU-ME@users.noreply.github.com>
2019-11-10 01:39:16 +0900

    Update yespower_sugarchain.c

--- yespower-1.0.1/yespower_sugarchain.c
+++ yespower-1.0.1/yespower_sugarchain.c
@@ -1,6 +1,6 @@
 #include "yespower-1.0.1/yespower.h"
 
-void yespower_hash(const char *input, char *output, uint32_t len) {
+void sugachain_hash(const char *input, char *output, uint32_t len) {
     static const yespower_params_t params = {
         .version = YESPOWER_1_0,
         .N = 2048,
