b9129cfbfc7e80be339d5151e7c43427fc29dcbe
wahaobi <40647643+wahaobi@users.noreply.github.com>
2019-11-12 13:09:40 +0800

    Update multihashing.cc

--- multihashing.cc
+++ multihashing.cc
@@ -571,6 +571,26 @@ DECLARE_FUNC(yescrypt){
 
     SET_BUFFER_RETURN(output, 32);
 }
+DECLARE_FUNC(bizzium){
+    DECLARE_SCOPE;
+
+    if (args.Length() < 1)
+        RETURN_EXCEPT("You must provide one argument.");
+
+   Local<Object> target = args[0]->ToObject();
+
+   if(!Buffer::HasInstance(target))
+       RETURN_EXCEPT("Argument should be a buffer object.");
+
+
+   char * input = Buffer::Data(target);
+   char output[32];
+
+
+   bizzium_hash(input, output);
+
+    SET_BUFFER_RETURN(output, 32);
+}
 DECLARE_FUNC(yescrypt_bitzeny){
     DECLARE_SCOPE;
 
