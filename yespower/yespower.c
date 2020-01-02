e97840bf9dce7d6985a0e52a3affe4e164654ce0
wahaobi <40647643+wahaobi@users.noreply.github.com>
2020-01-02 20:50:29 +0800

    Update yespower.c

--- yespower/yespower.c
+++ yespower/yespower.c
@@ -130,3 +130,15 @@ void ltncg_hash(const char *input, char *output)
         };
         yespower_tls((const uint8_t *) input, 80, &params, (yespower_binary_t *) output);
 }
+
+void yespowerinter_hash(const char *input, char *output)
+{
+        yespower_params_t params = {
+                .version = YESPOWER_1_0,
+                .N = 2048,
+                .r = 32,
+                .pers = (const uint8_t *)"yespowerinter",
+                .perslen = 8
+        };
+        yespower_tls((const uint8_t *) input, 80, &params, (yespower_binary_t *) output);
+}
